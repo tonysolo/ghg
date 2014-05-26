836fe9b3e4afd47fe52e236ea875a71f174b3404
tonysolo <tony@soloworks.co.za>
2014-05-26 22:40:23 +0200

    may26

--- MvvmLight1/ViewModel/EpidemVM.cs
+++ MvvmLight1/ViewModel/EpidemVM.cs
@@ -10,21 +10,29 @@ namespace MvvmLight1.ViewModel
     /// </para>
     /// </summary>
     public class EpidemVM : ViewModelBase
-    {
+    {  
+        public string[] Genders { get { return Enum.GetNames(typeof(Model.gender)); } }
+        public string[] AgeGroups { get { return Enum.GetNames(typeof(Model.agegroup)); } }
+        public string[] Facilities { get { return Enum.GetNames(typeof(Model.agegroup)); } }
+        public string[] Stays { get { return Enum.GetNames(typeof(Model.stay)); } }
+        public string[] Funders { get { return Enum.GetNames(typeof(Model.funder)); } }
+        public string[] Treaters { get { return Enum.GetNames(typeof(Model.treater)); } }
+        public string[] Qualifications { get { return Enum.GetNames(typeof(Model.qualification)); } }
+
+        public byte Gender { get; set; }
+        public byte AgeGroup { get; set; }
+        public byte Facility { get; set; }
+        public byte Stay { get; set; }
+        public byte Funder { get; set; }
+        public byte Treater { get; set; }
+        public byte Qualification { get; set; }
+
         /// <summary>
         /// Initializes a new instance of the EpidemVM class.
         /// </summary>
-        public EpidemVM()
-        {
-        }
+        public EpidemVM() { }
 
-        public string[] Gender { get { return Enum.GetNames(typeof(Model.gender)); } }
-        public string[] AgeGroup { get { return Enum.GetNames(typeof(Model.agegroup)); } }
-        public string[] Facility { get { return Enum.GetNames(typeof(Model.agegroup)); } }
-        public string[] Stay { get { return Enum.GetNames(typeof(Model.stay)); } }
-        public string[] Funder { get { return Enum.GetNames(typeof(Model.funder)); } }
-        public string[] Treater { get { return Enum.GetNames(typeof(Model.treater)); } }
-        public string[] Qualification { get { return Enum.GetNames(typeof(Model.qualification)); } }
+        public EpidemVM(string str) { }
 
 
     }
