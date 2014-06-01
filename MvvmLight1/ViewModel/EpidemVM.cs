1c4829b249f2a7c773a66ea1e73828ab5efac96d
tonysolo <tony@soloworks.co.za>
2014-06-01 19:31:41 +0200

    Sunday

--- MvvmLight1/ViewModel/EpidemVM.cs
+++ MvvmLight1/ViewModel/EpidemVM.cs
@@ -12,7 +12,7 @@ namespace MvvmLight1.ViewModel
     public class EpidemVM : ViewModelBase
     {
         public string[] Genders { get { return Enum.GetNames(typeof(Model.gender)); } }
-        public string[] AgeGroups {  get { return Enum.GetNames(typeof(Model.agegroup)); }  }     
+        public string[] AgeGroups { get { return Enum.GetNames(typeof(Model.agegroup)); } }
         public string[] Facilities { get { return Enum.GetNames(typeof(Model.facilty)); } }
         public string[] Stays { get { return Enum.GetNames(typeof(Model.stay)); } }
         public string[] Funders { get { return Enum.GetNames(typeof(Model.funder)); } }
@@ -20,15 +20,16 @@ namespace MvvmLight1.ViewModel
         public string[] Qualifications { get { return Enum.GetNames(typeof(Model.qualification)); } }
         public string[] Regions { get; set; } //will have to be set from map info
 
-       private byte _stay;
 
-public byte AgeGroup { get; set; }
-        public byte Gender { get; set; }       
+        public byte AgeGroup { get; set; }
+        public byte Gender { get; set; }
         public byte Facility { get; set; }
+
+        private byte _stay;
         public byte Stay
         {
-            get { return _stay; } 
-            set {_stay = value; RaisePropertyChanged("staymessage");}
+            get { return _stay; }
+            set { _stay = value; RaisePropertyChanged("staymessage"); }
         }
         public byte Funder { get; set; }
         public byte Treater { get; set; }
@@ -38,17 +39,13 @@ public byte AgeGroup { get; set; }
         public string TreaterRegion { get; set; }
 
         public string staymessage
-        {
-            get
-            {
-                return (Stay==1) ? "Number of Days Admitted":"Number of Similar Outpatients";
-            }
-        }
+        { get { return (Stay == 1) ? "Number of Days Admitted" : "Number of Similar Outpatients"; } }
+
         /// <summary>
         /// Initializes a new instance of the EpidemVM class.
         /// </summary>
         /// 
-        public EpidemVM()  { }
+        public EpidemVM() { }
 
 
 
