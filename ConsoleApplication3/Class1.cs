fcf708fd0e08c8d8e36ab782aacd7c9ede4fb9f5
tonysolo <tony@soloworks.co.za>
2014-07-21 11:13:20 +0200

    2107

--- ConsoleApplication3/Class1.cs
+++ ConsoleApplication3/Class1.cs
@@ -10,14 +10,15 @@ namespace ConsoleApplication3
     {
 public static int secstomidnight(string qe)
 {
+    DateTime.UtcNow.
     byte _qe = Convert.ToByte(qe, 16);    
     bool east = (_qe & 0x10) == 0;
     byte longit = (byte)(_qe & 0x07);
     int timezonesecs = longit * 45 * 60;
     timezonesecs = (east)? timezonesecs: timezonesecs *= -1;
-    DateTime dt = DateTime.UtcNow;
-    DateTime dt1 = dt.AddDays(1); 
-    TimeSpan ts = dt1 - dt;
+   // DateTime dt = DateTime.UtcNow;
+   // DateTime dt1 = dt.AddDays(1); 
+   // TimeSpan ts = dt1 - dt;
    
 return (timezonesecs);
 }
