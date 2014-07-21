237570dd54254ce840aefe7c2ea8e93ca68202f7
tonysolo <tony@soloworks.co.za>
2014-07-21 11:13:49 +0200

    2107

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -8,11 +8,14 @@ namespace ConsoleApplication3
 {
     class Program
     {
+        static public string[] tst ={"00","01","02","03","04","05","06","07","08","09","0A","0B","0C","0D","0E","0F",
+                                    "10","11","12","13","14","15","16","17","18","19","1A","1B","1C","1D","1E","1F"};
+       
         static void Main(string[] args)
         {
 
-          int x =  test.secstomidnight("37");
-          Console.WriteLine(x);
+         for (int i = 0;i<32;i++)
+          Console.WriteLine(test.secstomidnight(tst[i]));
           Console.ReadLine();
 
         }
