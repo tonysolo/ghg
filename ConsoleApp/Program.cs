dae59ac16309ba28323122cebf6bb1b9a1227aa7
tonysolo <tony@soloworks.co.za>
2014-08-18 12:04:58 +0200

    1808

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 8;
+
+            string s = "t-";
+            s = s + i.ToString();
+
+        }
+    }
+}
