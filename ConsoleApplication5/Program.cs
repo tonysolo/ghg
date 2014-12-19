00bda8c15b1f1bc3625f5a8f0fe31c64bf8130d2
tonysolo <tony@soloworks.co.za>
2014-12-19 21:56:43 +0200

    1912

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = '3';
+            var b = '6';
+            var c = b - a; 
+            
+            Console.Write(c);
+            Console.ReadLine();
+        }
+    }
+}
