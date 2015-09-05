c6bb7f599ebfd6af8e79b8346937e862442182d3
tonysolo <tony@soloworks.co.za>
2015-09-05 17:49:20 +0200

    0509

---
+++ MvvmLight1/Model/person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvvmLight1.Model
+{
+    public class person
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public string cellphone { get; set; }
+        public string email { get; set; }       
+    }
+
+}
